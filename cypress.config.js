97c235ae606e277ca8e40a941aac1f20bb09c192
Wisleymiguel <wisleymiguel4032x@gmail.com>
2025-11-27 13:12:16 -0300

    Initial commit - Projeto Cypress com Report Portal

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,30 @@
-module.exports = {
-  e2e: {
-    "baseUrl": "http://lojaebac.ebaconline.art.br",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-        require('cypress-html-reporter/GenerateReport')(on, config)
-    },
-  },
-};
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://lojaebac.ebaconline.art.br",
+    setupNodeEvents(on, config) {
+      // HTML Reporter
+      require('cypress-html-reporter/GenerateReport')(on, config);
+      
+      // Report Portal
+      require('@reportportal/agent-js-cypress/lib/plugin')(on, config);
+      
+      return config;
+    },
+  },
   reporter: '@reportportal/agent-js-cypress',
   reporterOptions: {
-    apiKey: '<API_KEY>',
+    apiKey: 'testecypress_WazL5x41Tb6KMHiuup17v3omviKWwrIcQWFVut55HeTGt0zP74469VFzypD1cced', // Substitua pela sua API Key
     endpoint: 'https://demo.reportportal.io/api/v1',
     project: 'wisleymiguel_personal',
     launch: 'testes',
-    description: 'teste ebac',}})
+    description: 'teste ebac',
+    attributes: [
+      {
+        key: 'ambiente',
+        value: 'homologacao'
+      }
+    ]
+  }
+});
